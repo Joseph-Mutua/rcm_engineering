6b3e27240c2a36b7dd615490830226ab1715d3a6
Joseph Mutua <Mutuaj793@gmail.com>
2022-05-09 16:45:11 +0300

    Finish up

--- stocks.py
+++ stocks.py
@@ -3,14 +3,15 @@ import json
 import csv
 
 apiKey = "c9sdv8qad3i4aps1sleg"
+
+# Ticker symbols for each stock
 symbols = ["AAPL", "AMZN", "NFLX", "GOOG", "FB"]
 
-
-# Get Quotes
 base_url="https://finnhub.io/api/v1/quote?"
 stocks = {}
 stocks_change = {}
 
+# Iterate through the symbols list and get the quote and percentage change for each stock
 for i in symbols:
     r = requests.get(base_url, params = {'symbol':i,'token':apiKey})
     text = r.text
@@ -19,19 +20,21 @@ for i in symbols:
     stocks[i] = stock
     stocks_change[i] = stock["dp"]
 
-
 greatest_change = max(stocks_change, key=stocks_change.get)
 
+# Return the stock with the greatest change from the stocks dictionary
 most_volatile_stock = (stocks[greatest_change])
 
+# Data rows for the csv file
 stock_symbol = greatest_change
 percentage_change = most_volatile_stock["dp"]
 current_price = most_volatile_stock["c"]
 last_close_price= most_volatile_stock["pc"]
 
-
+# Column headings for the csv file
 csv_columns = ['stock_symbol','percentage_change','current_price', "last_close_price"]
-# dict_data = {'stock_symbol' : stock_symbol, 'percentage_change': percentage_change,'current_price': current_price, "last_close_price": last_close_price}
+
+# Data to write to csv file
 csv_rows = [stock_symbol, percentage_change, current_price, last_close_price]
 csv_file = "stocks.csv"
 try:
